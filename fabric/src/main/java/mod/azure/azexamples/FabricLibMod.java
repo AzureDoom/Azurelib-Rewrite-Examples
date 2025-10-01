2ae1ba98db786a0785cb57671bae6598b022b152
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-10-01 13:00:56 -0400

    Built against 3.0.21

--- fabric/src/main/java/mod/azure/azexamples/FabricLibMod.java
+++ fabric/src/main/java/mod/azure/azexamples/FabricLibMod.java
@@ -1,14 +1,11 @@
 package mod.azure.azexamples;
 
-import mod.azure.azurelib.rewrite.animation.cache.AzIdentityRegistry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.item.Items;
 
 import mod.azure.azexamples.registry.EntityRegistry;
-import mod.azure.azexamples.registry.ItemRegistry;
 
 public final class FabricLibMod implements ModInitializer {
 
@@ -34,18 +31,6 @@ public final class FabricLibMod implements ModInitializer {
         FabricDefaultAttributeRegistry.register(
             EntityRegistry.MARINE.get(),
             Mob.createMobAttributes()
-        );
-        AzIdentityRegistry.register(
-            ItemRegistry.PISTOL.get(),
-            ItemRegistry.DOOMICORN_HELMET.get(),
-            ItemRegistry.DOOMICORN_CHESTPLATE.get(),
-            ItemRegistry.DOOMICORN_LEGGINGS.get(),
-            ItemRegistry.DOOMICORN_BOOTS.get(),
-            Items.NETHERITE_SWORD,
-            Items.NETHERITE_HELMET,
-            Items.NETHERITE_CHESTPLATE,
-            Items.NETHERITE_LEGGINGS,
-            Items.NETHERITE_BOOTS
         );
     }
 }
--- neoforge/src/main/java/mod/azure/azexamples/NeoForgeMod.java
+++ neoforge/src/main/java/mod/azure/azexamples/NeoForgeMod.java
@@ -1,23 +1,19 @@
 package mod.azure.azexamples;
 
-import mod.azure.azurelib.rewrite.animation.cache.AzIdentityRegistry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import mod.azure.azexamples.registry.EntityRegistry;
-import mod.azure.azexamples.registry.ItemRegistry;
 
 @Mod(CommonStrings.MOD_ID)
 public final class NeoForgeMod {
@@ -55,22 +51,6 @@ public final class NeoForgeMod {
         itemDeferredRegister.register(modEventBus);
         creativeModeTabDeferredRegister.register(modEventBus);
         modEventBus.addListener(this::createEntityAttributes);
-        modEventBus.addListener(this::commonSetup);
-    }
-
-    public void commonSetup(final FMLCommonSetupEvent event) {
-        AzIdentityRegistry.register(
-            ItemRegistry.PISTOL.get(),
-            ItemRegistry.DOOMICORN_HELMET.get(),
-            ItemRegistry.DOOMICORN_CHESTPLATE.get(),
-            ItemRegistry.DOOMICORN_LEGGINGS.get(),
-            ItemRegistry.DOOMICORN_BOOTS.get(),
-            Items.NETHERITE_SWORD,
-            Items.NETHERITE_HELMET,
-            Items.NETHERITE_CHESTPLATE,
-            Items.NETHERITE_LEGGINGS,
-            Items.NETHERITE_BOOTS
-        );
     }
 
     public void createEntityAttributes(final EntityAttributeCreationEvent event) {
