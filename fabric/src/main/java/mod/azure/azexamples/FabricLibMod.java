ef8c667112a32e58829e230a05aedc8ffb933407
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-10-01 12:58:30 -0400

    Built against 3.1.6

--- fabric/src/main/java/mod/azure/azexamples/FabricLibMod.java
+++ fabric/src/main/java/mod/azure/azexamples/FabricLibMod.java
@@ -1,11 +1,9 @@
 package mod.azure.azexamples;
 
-import mod.azure.azurelib.rewrite.animation.cache.AzIdentityRegistry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.item.Items;
 
 public final class FabricLibMod implements ModInitializer {
 
@@ -31,19 +29,6 @@ public final class FabricLibMod implements ModInitializer {
         FabricDefaultAttributeRegistry.register(
             ExampleRegistry.MARINE,
             Mob.createMobAttributes()
-        );
-        AzIdentityRegistry.register(
-            ExampleRegistry.PISTOL,
-            ExampleRegistry.STARGATE_ITEM,
-            ExampleRegistry.DOOMICORN_HELMET,
-            ExampleRegistry.DOOMICORN_CHESTPLATE,
-            ExampleRegistry.DOOMICORN_LEGGINGS,
-            ExampleRegistry.DOOMICORN_BOOTS,
-            Items.NETHERITE_SWORD,
-            Items.NETHERITE_HELMET,
-            Items.NETHERITE_CHESTPLATE,
-            Items.NETHERITE_LEGGINGS,
-            Items.NETHERITE_BOOTS
         );
     }
 }
--- forge/src/main/java/mod/azure/azexamples/ForgeMod.java
+++ forge/src/main/java/mod/azure/azexamples/ForgeMod.java
@@ -1,13 +1,10 @@
 package mod.azure.azexamples;
 
-import mod.azure.azurelib.rewrite.animation.cache.AzIdentityRegistry;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.item.Items;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod.EventBusSubscriber
@@ -21,23 +18,6 @@ public final class ForgeMod {
         ExampleRegistry.entityTypeDeferredRegister.register(modEventBus);
         ExampleRegistry.itemDeferredRegister.register(modEventBus);
         modEventBus.addListener(this::createEntityAttributes);
-        modEventBus.addListener(this::commonSetup);
-    }
-
-    public void commonSetup(final FMLCommonSetupEvent event) {
-        AzIdentityRegistry.register(
-            ExampleRegistry.PISTOL.get(),
-            ExampleRegistry.STARGATE_ITEM.get(),
-            ExampleRegistry.DOOMICORN_HELMET.get(),
-            ExampleRegistry.DOOMICORN_CHESTPLATE.get(),
-            ExampleRegistry.DOOMICORN_LEGGINGS.get(),
-            ExampleRegistry.DOOMICORN_BOOTS.get(),
-            Items.NETHERITE_SWORD,
-            Items.NETHERITE_HELMET,
-            Items.NETHERITE_CHESTPLATE,
-            Items.NETHERITE_LEGGINGS,
-            Items.NETHERITE_BOOTS
-        );
     }
 
     public void createEntityAttributes(final EntityAttributeCreationEvent event) {
