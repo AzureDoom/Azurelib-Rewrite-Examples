3a2c2d4170f7d296c6534b9815ff94cab73f95e6
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-10-06 07:05:56 -0400

    Back port missing mixins

---
+++ common/src/main/java/mod/azure/azexamples/mixins/VillagerMixin.java
@@ -0,0 +1,24 @@
+package mod.azure.azexamples.mixins;
+
+import mod.azure.azexamples.items.gunwitharm.GunWithArmItem;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Villager.class)
+public class VillagerMixin {
+
+    @Inject(at = @At("RETURN"), method = "mobInteract", cancellable = true)
+    private void azexamples$killVillager(Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir) {
+        final ItemStack itemStack = player.getItemInHand(hand);
+        if (itemStack.getItem() instanceof GunWithArmItem) {
+            cir.setReturnValue(InteractionResult.FAIL);
+        }
+    }
+}
